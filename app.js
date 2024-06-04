eb8cb433e5f43472886e99b9f61e6f85e528b9e4
aarontark <tarkington.aaron@gmail.com>
2024-06-04 05:28:34 -0500

    added variables for elements and for calculations

---
+++ app.js
@@ -0,0 +1,15 @@
+const billInput = document.querySelector('.bill-input');
+const tipSpans = document.querySelectorAll('.tip-span');
+const tipButtons = document.querySelectorAll('.tip-button');
+const customTip = document.querySelector('.custom-tip')
+const peopleInput = document.querySelector('.people-input');
+const errorMsg = document.querySelector('.error-msg');
+const resetBtn = document.querySelector('.reset-btn');
+const tipDisplay = document.querySelector('.total-tip');
+const totalDisplay = document.querySelector('.total-bill');
+
+let billTotal = 0;
+let numPeople = 0;
+let tipPercent = 0;
+
+
